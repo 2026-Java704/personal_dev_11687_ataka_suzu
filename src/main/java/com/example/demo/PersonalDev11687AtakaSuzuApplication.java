b7ed91ba0c3e4d7fe533cafa67591bc9ff102fef
student <student@FJ-1025>
2026-05-19 11:38:27 +0900

    コミット

---
+++ src/main/java/com/example/demo/PersonalDev11687AtakaSuzuApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalDev11687AtakaSuzuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalDev11687AtakaSuzuApplication.class, args);
+	}
+
+}
